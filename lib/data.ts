50fd1394695bd47b990a0ada285756e9af85fbfe
PietroMarrone01 <pietromarrone8@gmail.com>
2024-08-29 20:52:24 +0200

    projects

--- lib/data.ts
+++ lib/data.ts
@@ -4,6 +4,7 @@ import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import foodrecovery from "@/public/foodrecovery.png";
 import sdn from "@/public/sdn.png";
+import dectnr from "@/public/NRpluslogo.png";
 
 export const links = [
   {
@@ -61,13 +62,22 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "Food Recovery (Too Good To Go)",
+    title: "Master's Thesis",
+    description:
+      "Exploring Device-to-Device (D2D) Communication in Cellular Technology: 5G and Beyond",
+    tags: ["DECT NR+", "IoT", "5G", "D2D Communication", "Nordic Semiconductor", "nRF9161 DK"],
+    imageUrl: dectnr,
+    githubUrl: "",  // URL specifico per GitHub
+    documentationUrl: "https://drive.google.com/file/d/1rNGAPD0XaFuHnnd2DEj4MWty_5RrF_YM/view?usp=sharing",  
+  },
+  {
+    title: "Rescuing Surplus Food (Too Good To Go)",
     description:
       "Design and implementation of a web application aimed at recovering excess food from shops and restaurants.",
     tags: ["React", "Next.js", "SQLite", "Passport.js", "HTML", "CSS"],
     imageUrl: foodrecovery,
     githubUrl: "https://github.com/PietroMarrone01/Food-Recovery",  // URL specifico per GitHub
-    documentationUrl: "https://www.google.it",  
+    documentationUrl: "https://drive.google.com/file/d/1L_ZM8Sw6gFoAC3JW9WAMefKk7iHVtyZt/view?usp=sharing",  
   },
   {
     title: "Authentication Service - SDN",
@@ -76,7 +86,7 @@ export const projectsData = [
     tags: ["ONOS CONTROLLER", "MININET", "FLASK SERVER"],
     imageUrl: sdn,
     githubUrl: "https://github.com/PietroMarrone01/AUTHENTICATION-SERVICE-SDN",  // URL specifico per GitHub
-    documentationUrl: "https://www.google.it",  
+    documentationUrl: "https://drive.google.com/file/d/1ORRxMbRfCzZOtCUlXmuRx8Ak0C4awuXc/view?usp=sharing",  
   },
 ] as const;
 
