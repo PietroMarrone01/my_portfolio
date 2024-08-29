78db483276b306f9623b9e71671a9c5b9b3c4f17
PietroMarrone01 <pietromarrone8@gmail.com>
2024-08-29 12:13:02 +0200

    buttons4

--- components/project.tsx
+++ components/project.tsx
@@ -38,7 +38,7 @@ export default function Project({
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[24rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
@@ -59,7 +59,7 @@ export default function Project({
               href={githubUrl}
               target="_blank"
               rel="noopener noreferrer"
-              className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition"
+              className="text-white border border-gray-400 px-4 py-2 rounded-full bg-transparent hover:bg-gray-200 hover:text-black transition"
             >
               Github
             </a>
@@ -67,7 +67,7 @@ export default function Project({
               href={documentationUrl}
               target="_blank"
               rel="noopener noreferrer"
-              className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition"
+              className="text-white border border-gray-400 px-4 py-2 rounded-full bg-gray-200 hover:bg-gray-300 hover:text-black transition"
             >
               Documentation
             </a>
