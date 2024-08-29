f0d232d79c80ee0ca57d0fa87c866b7109e1cfc3
PietroMarrone01 <pietromarrone8@gmail.com>
2024-08-29 21:18:46 +0200

    final

--- components/project.tsx
+++ components/project.tsx
@@ -60,7 +60,7 @@ export default function Project({
                 href={githubUrl}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-gray-800 border border-gray-400 px-4 py-2 rounded-full bg-transparent hover:bg-gray-200 transition"
+                className="text-black border border-gray-400 px-4 py-2 rounded-full bg-gray-200 hover:bg-gray-300 transition"
               >
                 Github
               </a>
